ef35285c916efc14ba4c19074d147ec62ddb0253
Edison Alvarez <edisonalvarez32@gmail.com>
2025-09-26 11:49:42 -0500

    add pet

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace VeterinariaSanMiguel.Models;
 
-public class Pet
+public class Pet : Animal
 {
-    
+    public int IdPerson { get; set; }
+    [ForeignKey("IdPerson")]
+    public Client client { get; set; }
 }
